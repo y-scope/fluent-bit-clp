d9a1c4fcaa68f0260b38bf5f0539c034ce3b6cfb
Dave Marco <marcovitch.david@gmail.com>
2024-05-30 01:15:11 +0000

    setup basic stdout plugin, then realized need to use multi-instance, so switching now

--- plugins/out_clp_s3/out_clp_s3.go
+++ plugins/out_clp_s3/out_clp_s3.go
@@ -1,4 +1,100 @@
 package main
 
+import (
+	"C"
+	"fmt"
+	"time"
+	"unsafe"
+
+	"github.com/fluent/fluent-bit-go/output"
+)
+
+// Define the plugin context structure
+type pluginConfig struct {
+	path       string
+	file       string
+}
+
+//export FLBPluginRegister
+func FLBPluginRegister(def unsafe.Pointer) int {
+	return output.FLBPluginRegister(def, "out_clp_s3", "send logs to s3 in clp IR format")
+}
+
+//export FLBPluginInit
+// (fluentbit will call this)
+// plugin (context) pointer to fluentbit context (state/ c code)
+func FLBPluginInit(plugin unsafe.Pointer) int {
+	// Example to retrieve an optional configuration parameter
+	param := output.FLBPluginConfigKey(plugin, "param")
+	fmt.Printf("[flb-go] plugin parameter = '%s'\n", param)
+
+	//set context for plugin
+	path := output.FLBPluginConfigKey(plugin, "Path")
+	file := output.FLBPluginConfigKey(plugin, "File")
+	config := &pluginConfig{
+		path:       path,
+		file:       file,
+	}
+
+	output.FLBPluginSetContext(plugin, config)
+	return output.FLB_OK
+}
+
+//export FLBPluginFlush
+func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
+	var count int
+	var ret int
+	var ts interface{}
+	var record map[interface{}]interface{}
+
+	// Type assert context back into the original type for the Go variable
+	id := output.FLBPluginGetContext(ctx).(string)
+
+	// Create Fluent Bit decoder
+	dec := output.NewDecoder(data, int(length))
+
+	// Iterate Records
+	count = 0
+	for {
+		// Extract Record
+		ret, ts, record = output.GetRecord(dec)
+		if ret != 0 {
+			break
+		}
+
+		var timestamp time.Time
+		switch t := ts.(type) {
+		case output.FLBTime:
+			timestamp = ts.(output.FLBTime).Time
+		case uint64:
+			timestamp = time.Unix(int64(t), 0)
+		default:
+			fmt.Println("time provided invalid, defaulting to now.")
+			timestamp = time.Now()
+		}
+
+		// Print record keys and values
+		fmt.Printf("[%d] %s: [%s, {", count, C.GoString(tag),
+			timestamp.String())
+		for k, v := range record {
+			fmt.Printf("\"%s\": %v, ", k, v)
+		}
+		fmt.Printf("}\n")
+		count++
+	}
+
+	// Return options:
+	//
+	// output.FLB_OK    = data have been processed.
+	// output.FLB_ERROR = unrecoverable error, do not try this again.
+	// output.FLB_RETRY = retry to flush later.
+	return output.FLB_OK
+}
+
+//export FLBPluginExit
+func FLBPluginExit() int {
+	return output.FLB_OK
+}
+
 func main() {
 }
