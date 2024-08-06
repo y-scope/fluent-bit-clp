bce0d5b95c50485bbf97d6d42c7c6b01fd759965
Dave Marco <marcovitch.david@gmail.com>
2024-08-06 17:44:12 +0000

    minor changes and adding files

---
+++ plugins/out_clp_s3/internal/flush/flush.go
@@ -0,0 +1,208 @@
+// Package implements methods to send data to output. All data provided by Fluent Bit is encoded
+// with Msgpack.
+
+package flush
+
+import (
+	"C"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"time"
+	"unsafe"
+
+	"github.com/fluent/fluent-bit-go/output"
+	"github.com/ugorji/go/codec"
+	"github.com/y-scope/clp-ffi-go/ffi"
+
+	"github.com/y-scope/fluent-bit-clp/internal/decoder"
+	"github.com/y-scope/fluent-bit-clp/internal/outctx"
+)
+
+// Ingests new data from Fluent Bit chunk, then sends to s3 in IR format. Data may be buffered on
+// disk or in memory depending on plugin configuration.
+//
+// Parameters:
+//   - data: Msgpack data
+//   - size: Byte length
+//   - tag: Fluent Bit tag
+//   - ctx: Plugin context
+//
+// Returns:
+//   - code: Fluent Bit success code (OK, RETRY, ERROR)
+//   - err: Error if flush fails
+func Ingest(data unsafe.Pointer, size int, tagKey string, ctx *outctx.S3Context) (int, error) {
+	dec := decoder.New(data, size)
+	logEvents, err := decodeMsgpack(dec, ctx.Config)
+	if err != io.EOF {
+		return output.FLB_ERROR, err
+	}
+
+	tag, err := ctx.GetTag(tagKey, size)
+	if err != nil {
+		return output.FLB_RETRY, fmt.Errorf("error creating new tag: %w", err)
+	}
+
+	err = tag.Writer.WriteIrZstd(logEvents)
+	if err != nil {
+		return output.FLB_ERROR, err
+	}
+
+	readyToUpload, err := checkUploadCriteria(
+		tag,
+		ctx.Config.UseDiskBuffer,
+		ctx.Config.UploadSizeMb,
+	)
+	if err != nil {
+		return output.FLB_ERROR, fmt.Errorf("error checking upload criteria: %w", err)
+	}
+
+	if !readyToUpload {
+		return output.FLB_OK, nil
+	}
+
+	err = tag.ToS3(ctx.Config, ctx.Uploader)
+	if err != nil {
+		return output.FLB_ERROR, fmt.Errorf("error flushing Zstd buffer to s3: %w", err)
+	}
+
+	return output.FLB_OK, nil
+}
+
+// Decodes Msgpack Fluent Bit chunk into slice of log events. Decode of Msgpack based on
+// [Fluent Bit reference].
+//
+// Parameters:
+//   - decoder: Msgpack decoder
+//   - config: Plugin configuration
+//
+// Returns:
+//   - logEvents: Slice of log events
+//   - err: Error decoding Msgpack, error retrieving log message from decoded object
+//
+// [Fluent Bit reference]:
+// https://github.com/fluent/fluent-bit-go/blob/a7a013e2473cdf62d7320822658d5816b3063758/examples/out_multiinstance/out.go#L41
+func decodeMsgpack(dec *codec.Decoder, config outctx.S3Config) ([]ffi.LogEvent, error) {
+	var logEvents []ffi.LogEvent
+	for {
+		ts, record, err := decoder.GetRecord(dec)
+		if err != nil {
+			return logEvents, err
+		}
+
+		timestamp := decodeTs(ts)
+		msg, err := getMessage(record, config)
+		if err != nil {
+			err = fmt.Errorf("failed to get message from record: %w", err)
+			return nil, err
+		}
+
+		event := ffi.LogEvent{
+			LogMessage: msg,
+			Timestamp:  ffi.EpochTimeMs(timestamp.UnixMilli()),
+		}
+		logEvents = append(logEvents, event)
+	}
+}
+
+// Decodes timestamp provided by Fluent Bit engine into time.Time. If timestamp cannot be
+// decoded, returns system time.
+//
+// Parameters:
+//   - ts: Timestamp provided by Fluent Bit
+//
+// Returns:
+//   - timestamp: time.Time timestamp
+func decodeTs(ts any) time.Time {
+	var timestamp time.Time
+	switch t := ts.(type) {
+	case decoder.FlbTime:
+		timestamp = t.Time
+	case uint64:
+		timestamp = time.Unix(int64(t), 0)
+	default:
+		log.Printf("time provided invalid, defaulting to now. Invalid type is %T", t)
+		timestamp = time.Now()
+	}
+	return timestamp
+}
+
+// Retrieves message from a record object. The message can consist of the entire object or
+// just a single key. For a single key, user should set use_single_key to true in fluent-bit.conf.
+// In addition user, should set single_key to "log" which is default Fluent Bit key for unparsed
+// messages; however, single_key can be set to another value. To prevent failure if the key is
+// missing, user can specify allow_missing_key, and behaviour will fallback to the entire object.
+//
+// Parameters:
+//   - record: JSON record from Fluent Bit with variable amount of keys
+//   - config: Plugin configuration
+//
+// Returns:
+//   - msg: Retrieved message
+//   - err: Key not found, json.Unmarshal error, string type assertion error
+func getMessage(jsonRecord []byte, config outctx.S3Config) (string, error) {
+	if !config.UseSingleKey {
+		return string(jsonRecord), nil
+	}
+
+	var record map[string]any
+	err := json.Unmarshal(jsonRecord, &record)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal json record %v: %w", jsonRecord, err)
+	}
+
+	singleKeyMsg, ok := record[config.SingleKey]
+	if !ok {
+		// If key not found in record, see if allow_missing_key=true. If missing key is
+		// allowed, then return entire record.
+		if config.AllowMissingKey {
+			return string(jsonRecord), nil
+		}
+		return "", fmt.Errorf("key %s not found in record %v", config.SingleKey, record)
+	}
+
+	stringMsg, ok := singleKeyMsg.(string)
+	if !ok {
+		return "", fmt.Errorf("string type assertion for message failed %v", singleKeyMsg)
+	}
+
+	return stringMsg, nil
+}
+
+// Checks if criteria are met to upload to s3. If useDiskBuffer is false, then the chunk is always
+// uploaded so always returns true. If useDiskBuffer is true, check if Zstd buffer size is greater
+// than upload size.
+//
+// Parameters:
+//   - tag: Tag resources and metadata
+//   - useDiskBuffer: On/off for disk buffering
+//   - uploadSizeMb: S3 upload size in MB
+//
+// Returns:
+//   - readyToUpload: Boolean if upload criteria met or not
+//   - err: Error getting Zstd buffer size
+func checkUploadCriteria(tag *outctx.Tag, useDiskBuffer bool, uploadSizeMb int) (bool, error) {
+	if !useDiskBuffer {
+		return true, nil
+	}
+
+	_, bufferSize, err := tag.Writer.GetFileSizes()
+	if err != nil {
+		return false, fmt.Errorf("error could not get size of buffer: %w", err)
+	}
+
+	uploadSize := uploadSizeMb << 20
+
+	if bufferSize >= uploadSize {
+		log.Printf(
+			"Zstd buffer size of %d for tag %s exceeded upload size %d",
+			bufferSize,
+			tag.Key,
+			uploadSize,
+		)
+		return true, nil
+	}
+
+	return false, nil
+}
---
+++ plugins/out_clp_s3/internal/recovery/recovery.go
@@ -0,0 +1,265 @@
+// Package provides ability to recover disk buffer on startup and send to s3.
+
+package recovery
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/y-scope/fluent-bit-clp/internal/irzstd"
+	"github.com/y-scope/fluent-bit-clp/internal/outctx"
+)
+
+// If useDiskBuffer is set, close all files prior to exit. Graceful exit will only be called
+// if Fluent Bit receives a kill signal and not during an abrupt crash. Plugin is only
+// given a limited time to clean up resources, so stores are not sent to s3. Instead
+// they are sent during startup.
+//
+// Parameters:
+//   - ctx: Plugin context
+//
+// Returns:
+//   - err: Error closing file
+func GracefulExit(ctx *outctx.S3Context) error {
+	if !ctx.Config.UseDiskBuffer {
+		return nil
+	}
+
+	for _, tag := range ctx.Tags {
+		err := tag.Writer.Close()
+		if err != nil {
+			return err
+		}
+		tag.Writer = nil
+	}
+
+	return nil
+}
+
+// Sends existing disk buffers to S3.
+//
+// Parameters:
+//   - ctx: Plugin context
+//
+// Returns:
+//   - err: Error retrieving files, error files not valid, error flushing existing buffer
+func RecoverBufferFiles(ctx *outctx.S3Context) error {
+	irFiles, zstdFiles, err := getBufferFiles(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = checkFilesValid(irFiles, zstdFiles)
+	if err != nil {
+		return err
+	}
+
+	for tagKey, irFileInfo := range irFiles {
+		// Don't need to check ok return value since we already checked if key exists.
+		zstdFileInfo := zstdFiles[tagKey]
+		err := flushExistingBuffer(tagKey, irFileInfo, zstdFileInfo, ctx)
+		if err != nil {
+			return fmt.Errorf("error flushing existing buffer '%s': %w", tagKey, err)
+		}
+	}
+
+	return nil
+}
+
+// Retrieves fileInfo for every file in IR and Zstd disk buffer directories. For both IR and Zstd
+// directories, returns map with FluentBit tag as keys and FileInfo as values.
+//
+// Parameters:
+//   - ctx: Plugin context
+//
+// Returns:
+//   - irFiles: IR file map
+//   - ZstdFiles: Zstd file map
+//   - err: Error reading directory
+func getBufferFiles(
+	ctx *outctx.S3Context,
+) (map[string]os.FileInfo, map[string]os.FileInfo, error) {
+	irBufferPath, zstdBufferPath := getBufferPaths(ctx.Config)
+	irFiles, err := readDirectory(irBufferPath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	zstdFiles, err := readDirectory(zstdBufferPath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return irFiles, zstdFiles, nil
+}
+
+// Reads directory and returns map containing FileInfo for each file.
+//
+// Parameters:
+//   - dir: Path of disk buffer directory
+//
+// Returns:
+//   - files: Map with FileInfo for all files in buffer directory. Fluent Bit tag is map key.
+//   - err: Error reading directory, error retrieving FileInfo, error duplicate file
+func readDirectory(dir string) (map[string]os.FileInfo, error) {
+	files := make(map[string]os.FileInfo)
+
+	dirEntries, err := os.ReadDir(dir)
+	if os.IsNotExist(err) {
+		log.Printf("Recovered storage directory %s not found during startup", dir)
+		return files, nil
+	} else if err != nil {
+		return nil, fmt.Errorf("error reading directory '%s': %w", dir, err)
+	}
+
+	for _, dirEntry := range dirEntries {
+		fileInfo, err := getFileInfo(dirEntry)
+		if err != nil {
+			return nil, err
+		}
+		tagKey := strings.TrimSuffix(fileInfo.Name(), filepath.Ext(fileInfo.Name()))
+
+		if _, exists := files[tagKey]; !exists {
+			files[tagKey] = fileInfo
+		} else {
+			return nil, fmt.Errorf("error duplicate tag %s", tagKey)
+		}
+	}
+
+	return files, nil
+}
+
+// Gets fileInfo.
+//
+// Parameters:
+//   - dirEntry: Directory entry
+//
+// Returns:
+//   - fileInfo: FileInfo for the directory entry
+//   - err: Error retrieving fileInfo, error file is not a regular file
+func getFileInfo(dirEntry fs.DirEntry) (os.FileInfo, error) {
+	fileName := dirEntry.Name()
+	fileInfo, err := dirEntry.Info()
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving FileInfo for '%s': %w", fileName, err)
+	}
+
+	if !fileInfo.Mode().IsRegular() {
+		return nil, fmt.Errorf("error %s is not a regular file: %w", fileName, err)
+	}
+
+	return fileInfo, nil
+}
+
+// Checks if files in diskBufferDir are valid. After assertions, irFiles and zstdFiles are the same
+// length and have the same keys.
+//
+// Parameters:
+//   - irFiles: Map with FileInfo for all files in IR buffer directory. Fluent Bit tag is map key.
+//   - zstdFiles: Map with FileInfo for all files in Zstd buffer directory. Fluent Bit tag is map
+//     key.
+//
+// Returns:
+//   - err: Error files do not match
+func checkFilesValid(irFiles map[string]fs.FileInfo, zstdFiles map[string]fs.FileInfo) error {
+	if len(irFiles) != len(zstdFiles) {
+		return fmt.Errorf("error files in IR and Zstd store do not match")
+	}
+
+	for tagKey := range irFiles {
+		if _, ok := zstdFiles[tagKey]; !ok {
+			return fmt.Errorf("error files in IR and zstd store do not match")
+		}
+	}
+
+	return nil
+}
+
+// Flushes existing disk buffer to s3 on startup. Prior to sending, opens disk buffer files and
+// creates new [outctx.Tag] using existing buffer files. Removes IR preamble for new [outctx.Tag],
+// as existing stores should already have their own IR preamble.
+//
+// Parameters:
+//   - tagKey: Fluent Bit tag
+//   - irFileInfo: FileInfo for IR disk buffer file
+//   - zstdFileInfo: FileInfo for Zstd disk buffer file
+//   - ctx: Plugin context
+//
+// Returns:
+//   - err: error removing/open files, error creating tag, error flushing to s3
+func flushExistingBuffer(
+	tagKey string,
+	irFileInfo fs.FileInfo,
+	zstdFileInfo fs.FileInfo,
+	ctx *outctx.S3Context,
+) error {
+	irBufferPath, zstdBufferPath := getBufferPaths(ctx.Config)
+	irPath := filepath.Join(irBufferPath, irFileInfo.Name())
+	zstdPath := filepath.Join(zstdBufferPath, zstdFileInfo.Name())
+
+	irStoreSize := irFileInfo.Size()
+	zstdStoreSize := zstdFileInfo.Size()
+
+	if (irStoreSize == 0) && (zstdStoreSize == 0) {
+		err := removeBufferFiles(irPath, zstdPath)
+		// If both files are empty, and there is no error, it will skip tag. Creating unnecessary
+		// tag is wasteful. Also prevents accumulation of old tags no longer being sent by Fluent
+		// Bit.
+		return err
+	}
+
+	tag, err := ctx.RecoverTag(
+		tagKey,
+		int(irStoreSize),
+	)
+	if err != nil {
+		return fmt.Errorf("error recovering tag: %w", err)
+	}
+
+	log.Printf("Recovered stores for tag %s", tagKey)
+
+	err = tag.ToS3(ctx.Config, ctx.Uploader)
+	if err != nil {
+		return fmt.Errorf("error flushing Zstd store to s3: %w", err)
+	}
+
+	return nil
+}
+
+// Removes IR and Zstd disk buffer files.
+//
+// Parameters:
+//   - irPath: Path to IR disk buffer file
+//   - zstdPath: Path to Zstd disk buffer file
+//
+// Returns:
+//   - err: error removing files
+func removeBufferFiles(irPath string, zstdPath string) error {
+	err := os.Remove(irPath)
+	if err != nil {
+		return fmt.Errorf("error deleting file '%s': %w", irPath, err)
+	}
+	err = os.Remove(zstdPath)
+	if err != nil {
+		return fmt.Errorf("error deleting file '%s': %w", zstdPath, err)
+	}
+	return nil
+}
+
+// Retrieves paths for IR and Zstd disk buffer directories.
+//
+// Parameters:
+//   - config: Plugin config
+//
+// Returns:
+//   - irBufferPath: Path of IR disk buffer directory
+//   - zstdBufferPath: Path of Zstd disk buffer directory
+func getBufferPaths(config outctx.S3Config) (string, string) {
+	irBufferPath := filepath.Join(config.DiskBufferPath, irzstd.IrDir)
+	zstdBufferPath := filepath.Join(config.DiskBufferPath, irzstd.ZstdDir)
+	return irBufferPath, zstdBufferPath
+}
